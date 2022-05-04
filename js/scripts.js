9b5cafeb157474fbbad5ea8b6a6c8dec6a4f367e
josevega1223 <joseignaciovegapaulin@gmail.com>
2022-05-04 12:24:22 -0500

    Created one Pokemon object with 5 different keys

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 let pokemonList= [
+    {name: 'Bulbasaur', height:'2\'04"' , weight: '15.2 lbs', type:['grass','poison'] },
     {name: 'Ivysaur', height:'3\'03"' , weight: '28.7 lbs' , type:['grass', 'poison'] },
     
 
