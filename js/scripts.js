4b2974cce7faf102d43a7db5fcef96646c38fdc8
josevega1223 <joseignaciovegapaulin@gmail.com>
2022-05-04 12:57:15 -0500

    Created Pokemon Object with 5 keys
    Created the object of Charmeleon with its 5 keys

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ let pokemonList= [
     {name: 'Ivysaur', height:'3\'03"' , weight: '28.7 lbs' , type:['grass', 'poison'] },
     {name: 'Venusaur', height:'6\'07"' , weight: '220.5 lbs' , type:['grass', 'poison'] },
     {name: 'Charmander', height:'2\'00"', weight: '18.7 lbs' , type:'fire'},
+    {name: 'Charmeleon', height:'3\'07"', weight: '41.9 lbs', type:'fire'},
     
 
 ];
