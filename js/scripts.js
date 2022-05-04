3ebf62c20df7810f05bfd62ec2abe86b629f9dbc
josevega1223 <joseignaciovegapaulin@gmail.com>
2022-05-04 12:31:12 -0500

    Created Pokemon Object with 5 keys
    Created the object for Charmander with his keys

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ let pokemonList= [
     {name: 'Bulbasaur', height:'2\'04"' , weight: '15.2 lbs', type:['grass','poison'] },
     {name: 'Ivysaur', height:'3\'03"' , weight: '28.7 lbs' , type:['grass', 'poison'] },
     {name: 'Venusaur', height:'6\'07"' , weight: '220.5 lbs' , type:['grass', 'poison'] },
+    {name: 'Charmander', height:'2\'00"', weight: '18.7 lbs' , type:'fire'},
     
 
 ];
