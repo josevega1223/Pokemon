51504b9181042555196658dbd0f1bf3ef5a9bbfd
josevega1223 <joseignaciovegapaulin@gmail.com>
2022-05-04 13:17:43 -0500

    Added the Fifth Key that I missed
    I added the fifth key which is number to all 9 of the Pokemon objects that I created.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,12 @@
 let pokemonList= [
-    {name: 'Bulbasaur', height:'2\'04"' , weight: '15.2 lbs', type:['grass','poison'] },
-    {name: 'Ivysaur', height:'3\'03"' , weight: '28.7 lbs' , type:['grass', 'poison'] },
-    {name: 'Venusaur', height:'6\'07"' , weight: '220.5 lbs' , type:['grass', 'poison'] },
-    {name: 'Charmander', height:'2\'00"', weight: '18.7 lbs' , type:'fire'},
-    {name: 'Charmeleon', height:'3\'07"', weight: '41.9 lbs', type:'fire'},
-    {name: 'Charizard', height:'5\'07"', weight: '199.5 lbs', type:['fire', 'flying']},
-    
-    {name: 'Squirtle', height:'1\'08"', weight: '19.8 lbs', type:'water'},
-    {name: 'Wartortle', height:'3\'03"', weight: '49.6 lbs', type:'water'},
-    {name: 'Blastoise', height:'5\'03"', weight: '188.5 lbs', type:'water'}
+    {name: 'Bulbasaur', height:'2\'04"' , weight: '15.2 lbs', type:['grass','poison'], number:'#001' },
+    {name: 'Ivysaur', height:'3\'03"' , weight: '28.7 lbs' , type:['grass', 'poison'], number:'#002' },
+    {name: 'Venusaur', height:'6\'07"' , weight: '220.5 lbs' , type:['grass', 'poison'], number:'#003' },
+    {name: 'Charmander', height:'2\'00"', weight: '18.7 lbs' , type:'fire', number:'#004'},
+    {name: 'Charmeleon', height:'3\'07"', weight: '41.9 lbs', type:'fire', number:'#005'},
+    {name: 'Charizard', height:'5\'07"', weight: '199.5 lbs', type:['fire', 'flying'], number:'#006'},
+    {name: 'Squirtle', height:'1\'08"', weight: '19.8 lbs', type:'water', number:'#007'},
+    {name: 'Wartortle', height:'3\'03"', weight: '49.6 lbs', type:'water', number:'#008'},
+    {name: 'Blastoise', height:'5\'03"', weight: '188.5 lbs', type:'water', number:'#009'}
 
 ];
