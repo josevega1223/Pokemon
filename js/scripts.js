7ea152a59f7e9ed2a44142213230d6f34c6b042b
josevega1223 <joseignaciovegapaulin@gmail.com>
2022-05-04 12:30:27 -0500

    Created Pokemon object with 5 keys
    Created the Object for Venusaur

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 let pokemonList= [
     {name: 'Bulbasaur', height:'2\'04"' , weight: '15.2 lbs', type:['grass','poison'] },
     {name: 'Ivysaur', height:'3\'03"' , weight: '28.7 lbs' , type:['grass', 'poison'] },
+    {name: 'Venusaur', height:'6\'07"' , weight: '220.5 lbs' , type:['grass', 'poison'] },
     
 
 ];
